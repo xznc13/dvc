82b60c6bb9490ef9f1c8648f07c5239337d44def
xznc13 <stephen.butcher@outlook.com>
2016-02-26 08:52:32 +0000

    Added Static Data script and user data

---
+++ container-files/initdbscripts/common/004-Insert-Static-Data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+if [ "${DBNAME}" == "" ]
+    then
+	/usr/bin/echo "The ENV variable DBNAME has not been defined, so exiting"
+    else
+	/usr/bin/echo "Found variable DBNAME pointing to ${DBNAME}, continuing"
+	for i in /initdbscripts/common/Static/*
+	  do
+	   /usr/bin/echo "$0: running $i"
+	   /usr/bin/mysql -D ${DBNAME} < $i
+	  done
+fi
---
+++ container-files/initdbscripts/common/Static/static_user_login.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user_login` VALUES (4,1,'user','user','user',1),(5,2,'admin','admin','admin',2),(6,3,'rule','rule','rulesexpert',3);
